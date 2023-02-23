f3afa13283bf894131ca95ba918e3aa4e21ec37e
Javier Azua <azua.javi@gmail.com>
2023-02-23 16:21:25 -0300

    added thumnail direction

--- app/head.tsx
+++ app/head.tsx
@@ -8,7 +8,10 @@ export default function Head() {
         name="description"
         content="Looking for a skilled video editor who can help you bring your vision to life? With years of experience in video editing and a background in cinematic arts, I have the skills and expertise to create visually stunning videos that communicate your message effectively."
       />
-      <meta property="og:image" content="/thumbnail.jpg" />
+      <meta
+        property="og:image"
+        content="https://www.gonzaloagostino.com/thumbnail.jpg"
+      />
       <link rel="icon" href="/favicon.ico" />
     </>
   );
