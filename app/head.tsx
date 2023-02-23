c67d3efe31a3511d891a0ebabc1a3305b2c7938e
Javier Azua <azua.javi@gmail.com>
2023-02-23 16:18:37 -0300

    added thumnail and description

--- app/head.tsx
+++ app/head.tsx
@@ -3,11 +3,12 @@ export default function Head() {
     <>
       <title>Gonzalo Agostino</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta property="og:title" content="Gonzalo Agostino Portfolio" />
       <meta
         name="description"
-        content="Portfolio of Gonzalo Agostino, made with NextJS 13"
+        content="Looking for a skilled video editor who can help you bring your vision to life? With years of experience in video editing and a background in cinematic arts, I have the skills and expertise to create visually stunning videos that communicate your message effectively."
       />
-      <meta property="og:image" content="" />
+      <meta property="og:image" content="/thumbnail.jpg" />
       <link rel="icon" href="/favicon.ico" />
     </>
   );
