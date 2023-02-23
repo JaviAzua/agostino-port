0fa6ba93ae05950842bffc27c14920fb2bf187fc
Javier Azua <azua.javi@gmail.com>
2023-02-23 16:27:44 -0300

    added meta tags

--- app/head.tsx
+++ app/head.tsx
@@ -9,9 +9,15 @@ export default function Head() {
         content="Looking for a skilled video editor who can help you bring your vision to life? With years of experience in video editing and a background in cinematic arts, I have the skills and expertise to create visually stunning videos that communicate your message effectively."
       />
       <meta
+        name="og:description"
+        content="Looking for a skilled video editor who can help you bring your vision to life? With years of experience in video editing and a background in cinematic arts, I have the skills and expertise to create visually stunning videos that communicate your message effectively."
+      />
+      <meta
         property="og:image"
         content="https://www.gonzaloagostino.com/thumbnail.jpg"
       />
+      <meta property="og:type" content="profile" />
+      <meta property="og:url" content="https://www.gonzaloagostino.com/" />
       <link rel="icon" href="/favicon.ico" />
     </>
   );
